f7e3875b613ff9e79fe27c6272f8167f9e96a5b8
Putrus <puertaputra@gmail.com>
2021-12-05 14:34:31 +0100

    Day5 is done!

---
+++ Day5/HydrotermalVenture.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+struct Line {
+   int x1, y1, x2, y2;
+};
+
+
+class HydrotermalVenture {
+private:
+   std::vector<std::vector<int>> diagram;
+   std::vector<Line> lines;
+public:
+   HydrotermalVenture();
+   void loadInput(const char* filename);
+   std::vector<int> split(std::string str);
+   int getResultPart1();
+   void printDiagram();
+};
--- main.cpp
+++ main.cpp
@@ -4,21 +4,11 @@
 #include "Day2/Dive.h"
 #include "Day3/BinaryDiagnostic.h"
 #include "Day4/GiantSquid.h"
+#include "Day5/HydrotermalVenture.h"
 
 int main() {
-   ///*SonarSweep s;
-   //s.loadInput("Day1/SonarSweepInput");
-   //std::cout << s.getResultPart2() << std::endl;*/
-   //GiantSquid b;
-   //b.loadInput("Day4/GiantSquidInput.txt");
-   //std::cout << b.getResultPart2();
-   ////b.test();
-   ////std::cout << b.getResultPart2();
-   ///*Board c({ {14, 21, 17, 24, 4},
-   //   {10, 16, 15 ,9 , 19},
-   //   {18, 8, 23, 26, 20},
-   //   {22, 11, 13, 6, 5},
-   //   {2, 0, 12, 3, 7} });
-   //std::vector<int> r = c.getUnmarkedWin({ 7,4,9,5,11,17,23,2,0,14,21,24 });*/
+   HydrotermalVenture h;
+   h.loadInput("Day5/Input.txt");
+   std::cout << h.getResultPart1();
    return 0;
 }
