30c0994ba28fa492abd00d54734e47f5bc7fc252
Marcin Putra <marcin.putra.0@gmail.com>
2022-12-17 17:16:47 +0100

    Day 13 std::variant

--- AdventOfCode2022/Day13/inc/DistressSignal.h
+++ AdventOfCode2022/Day13/inc/DistressSignal.h
@@ -4,6 +4,39 @@
 
 #include <vector>
 #include <string>
+#include <variant>
+
+struct Packet : std::variant<int, std::vector<Packet>>
+{
+   Packet(int val);
+   Packet(std::vector<Packet> const& vec);
+
+   friend bool operator<(Packet const& lhs, Packet const& rhs);
+};
+
+struct CompareVisitor
+{
+   bool operator()(int lhs, int rhs) const
+   {
+      return lhs < rhs;
+   }
+
+   bool operator()(std::vector<Packet> const& lhs, std::vector<Packet> const& rhs)
+   {
+      return std::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+   }
+
+   bool operator()(int lhs, std::vector<Packet> const& rhs)
+   {
+      return (*this)({ lhs }, rhs);
+   }
+
+   bool operator()(std::vector<Packet> const& lhs, int rhs)
+   {
+      return (*this)(lhs, { rhs });
+   }
+
+};
 
 class DistressSignal final : public PuzzleDecorator
 {
@@ -15,6 +48,7 @@ private:
    void loadInput(const char* input_path) override;
    void calcAndPrintAnswerToPart1() override;
    void calcAndPrintAnswerToPart2() override;
+   Packet parse(std::string& s) const;
 
 private:
    std::vector<std::pair<std::string, std::string>> pairs;
--- AdventOfCode2022/Day13/src/DistressSignal.cpp
+++ AdventOfCode2022/Day13/src/DistressSignal.cpp
@@ -3,6 +3,18 @@
 #include <fstream>
 #include <iostream>
 
+
+Packet::Packet(int val) : variant(val)
+{}
+
+Packet::Packet(std::vector<Packet> const& vec) : variant(vec)
+{}
+
+bool operator<(Packet const& lhs, Packet const& rhs)
+{
+   return std::visit(CompareVisitor(), lhs, rhs);
+}
+
 DistressSignal::DistressSignal(std::shared_ptr<PuzzleComponent> puzzle) : PuzzleDecorator(puzzle)
 {}
 
@@ -41,10 +53,48 @@ void DistressSignal::loadInput(const char* input_path)
 
 void DistressSignal::calcAndPrintAnswerToPart1()
 {
-
+   for (auto pair : pairs)
+   {
+      Packet first = parse(pair.first);
+      Packet second = parse(pair.second);
+   }
 }
 
 void DistressSignal::calcAndPrintAnswerToPart2()
 {
 
+}
+
+Packet DistressSignal::parse(std::string& s) const
+{
+   if (s[0] == '[' && s[1] == ']')
+   {
+      return Packet({});
+   }
+
+   if (s[0] == '[')
+   {
+      std::vector<Packet> vec;
+      while (s[0] != ']')
+      {
+         s = s.substr(1, s.length() - 1);
+         vec.push_back(parse(s));
+      }
+      s = s.substr(1, s.length() - 1);
+      return vec;
+   }
+   size_t comma_pos = s.find(',');
+   size_t bracket_pos = s.find(']');
+   if (comma_pos < bracket_pos || comma_pos == std::string::npos)
+   {
+      s = s.substr(comma_pos, s.length() - comma_pos);
+      return stoi(s.substr(comma_pos));
+   }
+   else if(comma_pos > bracket_pos || bracket_pos == std::string::npos)
+   {
+      s = s.substr(bracket_pos, s.length() - bracket_pos);
+      return stoi(s.substr(bracket_pos));
+   }
+   
+   return stoi(s);
 }
